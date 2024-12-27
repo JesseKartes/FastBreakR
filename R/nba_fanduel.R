41d46b66609e613949047eff4f38a7cd64f8da94
Jesse Kartes <jesse.kartes@gmail.com>
2024-12-26 18:46:44 -0600

    Create nba_fanduel.R

---
+++ R/nba_fanduel.R
@@ -0,0 +1,79 @@
+#' Get NBA FanDuel Data
+#'
+#' This function gets FanDuel data for a vector of game IDs and returns a
+#' combined tibble.
+#' Creates batches of `game_ids` and pauses between batches to avoid timeout issues.
+#'
+#' @param game_ids A character vector of game IDs.
+#' @param batch_size Number of requests before pausing (default: 100)
+#' @param pause_seconds Number of seconds to pause between batches (default: 15)
+#' @return A tibble containing combined FanDuel data for all game IDs.
+#' @export
+nba_fanduel <- function(game_ids, batch_size = 100, pause_seconds = 15) {
+    unique_games <- unique(game_ids)
+    total_games <- length(unique_games)
+
+    # Divide game IDs into batches
+    batched_games <- split(unique_games, ceiling(seq_along(unique_games) / batch_size))
+    num_batches <- length(batched_games)
+
+    message(glue::glue("Processing {total_games} games in {num_batches} batches"))
+
+    future::plan(future::multisession)
+
+    # Process each batch sequentially with parallel handling within batches
+    results <- map_dfr(seq_along(batched_games), function(batch_num) {
+        batch_games <- batched_games[[batch_num]]
+        message(glue::glue("Processing batch {batch_num}/{num_batches}: games {batch_games[1]} to {batch_games[length(batch_games)]}"))
+
+        # Fetch data in parallel for the current batch
+        batch_results <- future_map_dfr(batch_games, ~ {
+            tryCatch({
+                fetch_fanduel_data(.x)
+
+            }, error = function(e) {
+                message(glue::glue("Error fetching data for Game ID {.x}: {e$message}"))
+                return(tibble())
+            })
+        })
+
+        # Pause after processing a batch unless it's the last batch
+        if (batch_num < num_batches) {
+            message(glue::glue("Pausing for {pause_seconds} seconds..."))
+            Sys.sleep(pause_seconds)
+        }
+
+        return(batch_results)
+    })
+
+    return(results)
+}
+
+#' Fetch FanDuel Data from API
+#'
+#' This function fetches FanDuel data for a given game ID.
+#'
+#' @param game_id The ID of the game for which to fetch data.
+#' @return A list containing the raw FanDuel data and column names.
+fetch_fanduel_data <- function(game_id) {
+    headers <- generate_headers_stats()
+
+    all_data <- map_dfr(game_id, function(game) {
+        url <- paste0("https://stats.nba.com/stats/infographicfanduelplayer/?gameId=", game)
+
+        data <- get_data_no_params(url, headers)
+
+        column_names <- data$resultSets$headers[[1]] %>%
+            as.character()
+
+        data <- data$resultSets$rowSet[[1]] %>%
+            data.frame(stringsAsFactors = FALSE) %>%
+            as_tibble() %>%
+            set_names(column_names) %>%
+            clean_names()
+
+        return(data)
+    })
+
+    return(all_data)
+}
