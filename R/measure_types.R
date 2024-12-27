82cc916d818595ecfe36105ba0f6947747141d8e
Jesse Kartes <jesse.kartes@gmail.com>
2024-12-26 18:46:38 -0600

    Create measure_types.R

---
+++ R/measure_types.R
@@ -0,0 +1,25 @@
+#' Measure Types for NBA Team Stats
+#'
+#' This file contains the available measure types for NBA team statistics.
+#' The measure types can be used to filter or categorize team stats in various
+#' analyses and models.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name team_measure_types
+#' @format A character vector.
+#' @source FastBreakR
+team_measure_types <- c("Base", "Advanced", "Four Factors", "Misc", "Scoring")
+
+#' Measure Types for NBA Player Stats
+#'
+#' This file contains the available measure types for NBA player statistics.
+#' The measure types can be used to filter or categorize player stats in various
+#' analyses and models.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name player_measure_types
+#' @format A character vector.
+#' @source FastBreakR
+player_measure_types <- c("Base", "Advanced", "Usage", "Misc", "Scoring")
