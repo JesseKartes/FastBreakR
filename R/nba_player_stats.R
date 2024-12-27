c3637fae30cca4aeeaa1c9d0e36d5f67227b472b
Jesse Kartes <jesse.kartes@gmail.com>
2024-12-26 20:27:44 -0600

    Update nba_player_stats.R

--- R/nba_player_stats.R
+++ R/nba_player_stats.R
@@ -9,42 +9,44 @@
 #' @return A named list where each element is a data frame containing player level stats for that season.
 #' @export
 nba_player_stats <- function(seasons, season_type = "Regular Season") {
-    if (!is.numeric(seasons) || length(seasons) == 0) {
-        stop("The 'seasons' parameter must be a non-empty numeric vector.")
-    }
+  if (!is.numeric(seasons) || length(seasons) == 0) {
+    stop("The 'seasons' parameter must be a non-empty numeric vector.")
+  }
 
-    results <- map(seq_along(seasons), function(i) {
-        season <- seasons[i]
-        message(glue::glue("Processing season {season} ({i}/{length(seasons)})"))
+  results <- map(seq_along(seasons), function(i) {
+    season <- seasons[i]
+    message(glue::glue("Processing season {season} ({i}/{length(seasons)})"))
 
-        # Try to fetch and process data for the season
-        nba_final <- tryCatch({
-            all_data_list <- map(player_measure_types, function(measure_type) {
-                fetch_player_stats(season, measure_type, season_type)
-            })
-
-            all_data_list <- map(all_data_list, process_player_measures)
-
-        }, error = function(e) {
-            message(glue::glue("Error processing season {season}: {e$message}"))
-            return(NULL)  # Return NULL if an error occurs
+    # Try to fetch and process data for the season
+    nba_final <- tryCatch(
+      {
+        all_data_list <- map(player_measure_types, function(measure_type) {
+          fetch_player_stats(season, measure_type, season_type)
         })
 
-        # Pause after processing each season unless it's the last
-        if (i < length(seasons)) {
-            message(glue::glue("Pausing for 5 seconds before processing the next season..."))
-            Sys.sleep(5)
-        }
+        all_data_list <- map(all_data_list, process_player_measures)
+      },
+      error = function(e) {
+        message(glue::glue("Error processing season {season}: {e$message}"))
+        return(NULL) # Return NULL if an error occurs
+      }
+    )
 
-        if (!is.null(nba_final)) {
-            names(nba_final) <- get_player_measure_types()
-        }
+    # Pause after processing each season unless it's the last
+    if (i < length(seasons)) {
+      message(glue::glue("Pausing for 5 seconds before processing the next season..."))
+      Sys.sleep(5)
+    }
 
-        return(nba_final)
-    })
+    if (!is.null(nba_final)) {
+      names(nba_final) <- get_player_measure_types()
+    }
 
-    names(results) <- glue::glue("season_{seasons}") %>% as.character()
-    return(results)
+    return(nba_final)
+  })
+
+  names(results) <- glue::glue("season_{seasons}") %>% as.character()
+  return(results)
 }
 
 #' Fetch NBA Player Stats from API
@@ -56,24 +58,24 @@ nba_player_stats <- function(seasons, season_type = "Regular Season") {
 #' @param season_type A character string specifying the type of season (e.g., "Regular Season").
 #' @return A data frame with cleaned NBA player stats.
 fetch_player_stats <- function(season, measure_type, season_type) {
-    headers <- generate_headers_stats()
+  headers <- generate_headers_stats()
 
-    url <- "https://stats.nba.com/stats/playergamelogs"
+  url <- "https://stats.nba.com/stats/playergamelogs"
 
-    params <- generate_parameters_stats(season, measure_type, season_type)
+  params <- generate_parameters_stats(season, measure_type, season_type)
 
-    data <- get_data(url, headers, params)
+  data <- get_data(url, headers, params)
 
-    column_names <- data$resultSets$headers[[1]] %>%
-        as.character()
+  column_names <- data$resultSets$headers[[1]] %>%
+    as.character()
 
-    dt <- data$resultSets$rowSet[[1]] %>%
-        data.frame(stringsAsFactors = FALSE) %>%
-        as_tibble() %>%
-        set_names(column_names) %>%
-        clean_names()
+  dt <- data$resultSets$rowSet[[1]] %>%
+    data.frame(stringsAsFactors = FALSE) %>%
+    as_tibble() %>%
+    set_names(column_names) %>%
+    clean_names()
 
-    return(clean_stats_cols(dt))
+  return(clean_stats_cols(dt))
 }
 
 #' Process Player Data
@@ -86,15 +88,15 @@ fetch_player_stats <- function(season, measure_type, season_type) {
 #' relevant columns converted to numeric.
 #' @export
 process_player_measures <- function(player_data) {
-    player_data <- player_data %>%
-        mutate(game_date = as_date(game_date)) %>%
-        arrange(game_date, game_id, player_id)
+  player_data <- player_data %>%
+    mutate(game_date = as_date(game_date)) %>%
+    arrange(game_date, game_id, player_id)
 
-    min_column_index <- which(names(player_data) == "min")
-    cols_to_convert <- min_column_index:(ncol(player_data) - 1)
+  min_column_index <- which(names(player_data) == "min")
+  cols_to_convert <- min_column_index:(ncol(player_data) - 1)
 
-    player_data <- player_data %>%
-        mutate(across(cols_to_convert, as.numeric))
+  player_data <- player_data %>%
+    mutate(across(cols_to_convert, as.numeric))
 
-    return(player_data)
+  return(player_data)
 }
