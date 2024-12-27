e682779c33742c8b397e0b4e5204b1405612bdff
Jesse Kartes <jesse.kartes@gmail.com>
2024-12-26 20:27:34 -0600

    Update globals.R

--- R/globals.R
+++ R/globals.R
@@ -1,14 +1,14 @@
 # Declare global variables used across the package to avoid check warnings
 utils::globalVariables(c(
-    ".", "Q1", "days_next_game", "days_rest", "fg_pct", "fga", "fgm",
-    "gameID", "game_count", "game_count_season", "game_date", "game_date_est",
-    "game_id", "game_label", "game_list", "inBonus", "is_b2b", "is_b2b_first",
-    "is_b2b_second", "loc_x", "loc_y", "matchup", "opp_days_next_game",
-    "opp_days_rest", "opp_game_count", "opp_game_count_season", "opp_is_b2b",
-    "opp_is_b2b_first", "opp_is_b2b_second", "opp_pts", "opp_team_abbreviation",
-    "opp_team_id", "opp_team_name", "periodType", "player_id", "plus_minus",
-    "process_team_games", "pts", "score", "season_year", "shot_attempted_flag",
-    "shot_distance", "shot_made_flag", "shot_type", "shot_zone_basic",
-    "teamName", "team_abbreviation", "team_city", "team_id", "team_name",
-    "timeoutsRemaining", "wl", "statistics"
+  ".", "Q1", "days_next_game", "days_rest", "fg_pct", "fga", "fgm",
+  "gameID", "game_count", "game_count_season", "game_date", "game_date_est",
+  "game_id", "game_label", "game_list", "inBonus", "is_b2b", "is_b2b_first",
+  "is_b2b_second", "loc_x", "loc_y", "matchup", "opp_days_next_game",
+  "opp_days_rest", "opp_game_count", "opp_game_count_season", "opp_is_b2b",
+  "opp_is_b2b_first", "opp_is_b2b_second", "opp_pts", "opp_team_abbreviation",
+  "opp_team_id", "opp_team_name", "periodType", "player_id", "plus_minus",
+  "process_team_games", "pts", "score", "season_year", "shot_attempted_flag",
+  "shot_distance", "shot_made_flag", "shot_type", "shot_zone_basic",
+  "teamName", "team_abbreviation", "team_city", "team_id", "team_name",
+  "timeoutsRemaining", "wl", "statistics"
 ))
